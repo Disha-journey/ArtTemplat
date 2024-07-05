4075912cbefc09bb43314d0406fcecdb830b4c68
Disha-journey <dishadisha2305@gmail.com>
2024-07-05 12:56:36 +0530

    Add files via upload

---
+++ template.php
@@ -0,0 +1,50 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styletemplate.css">
+
+</head>
+<body>
+    <div class="main">
+        <div class="top">
+            <h1 class="left">My Art</h1>
+            <h1 class="right"><a href="#About">About</a></h1>
+        </div>
+    <div class="center">
+        <div class="lefthalf">
+            <img src="images (1).jpg" width="49%" height="34%">
+            <img src="images (2).jpg" width="50%" height="34%">
+            <img src="images (3).jpg" width="49%" height="34%">
+            <img src="images.jpg" width="50%" height="34%">
+            <img src="img1.jpg" width="49%" height="32%">
+            <img src="image2.jpg" width="50%" height="32%">
+        </div>
+    
+        <div class="righthalf">
+            <img src="images (6).jpg" width="99%" height="50%">
+            <img src="images (5).jpg" width="50%" height="50%" >
+            <img src="images (7).jpg" width="49%" height="50%" >
+        </div>
+    </div>
+    <div class="bottom">
+        <h2><a name="About">About</h2>
+        <img class="picture" src="pic.jpg" width="200px" height="300px">
+        <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+        </p>
+        <h4 style="color: red;">Do not hesitate to contact me!!</h4>
+        <form action="collectdata.php" method="post">
+            <b>Name</b>
+            <input class="one" type="text" name="name">
+            <b>Email</b>
+            <input class="one" type="email" name="email">
+            <b>Message</b>
+            <input class="one" type="text" name="msg">
+            <input class="one two" type="submit" name="send" value="Send">
+        </form>
+
+    </div>
+    </div>
+</body>
+</html>
